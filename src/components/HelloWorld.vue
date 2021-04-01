758b71897cc7093e071a30b90639405ff1e5be42
junwei <1157593964@qq.com>
2021-04-01 17:47:03 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <div class="left">
+            <iframe
+                src="https://blog.csdn.net/vivianluolita/article/details/53689745"
+                frameborder="0"
+                width="100%"
+                height="100%"
+                @select="selectHandle"
+                id="testIframe"
+            ></iframe>
+            <!-- <img src="../assets/logo.png" alt="" /> -->
+        </div>
+        <div
+            class="right"
+            ref="dragTarget"
+            @dragenter="testDrag"
+            @dragover="dragoverHandler"
+            @drop.prevent="testDrag"
+        >
+            我是可拖拽区域
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    mounted() {
+        let iframe = document.getElementById('testIframe');
+        iframe.onload = function() {
+            // let i = iframe.contentWindow.document.getElementById('head');
+            // let i = iframe.contentDocument.getElementById('head');
+            // console.log(
+            //     iframe.contentWindow.document.getElementById('csdn-toolbar')
+            // );
+            // iframe.onclick = function() {
+            //     console.log('====', 222);
+            // };
+            // iframe.contentDocument.addEventListener(
+            //     'click',
+            //     function() {
+            //         alert(222);
+            //     },
+            //     false
+            // );
+            // iframe.contentWindow.onclick = function() {
+            //     console.log('====', 222);
+            // };
+            // iframe.contentDocument.onclick = function() {
+            //     console.log('4444444444');
+            // };
+
+            window.addEventListener('click', function() {
+                console.log('2222');
+            });
+        };
+    },
+    methods: {
+        selectHandle(e) {
+            console.log(e);
+        },
+        dragoverHandler(event) {
+            event.preventDefault();
+        },
+        testDrag(e) {
+            console.log(e);
+        },
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+    display: flex;
+    height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.left {
+    width: 80%;
+    height: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.right {
+    width: 20%;
+    background: gray;
+    height: 100%;
 }
 </style>
